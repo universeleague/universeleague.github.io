c6ce613bbc628fae454883ec4e10014d9a03ff74
universeleague <69162498+universeleague@users.noreply.github.com>
2024-11-18 00:39:36 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -87,7 +87,7 @@ function convertCSVArrayToTraineeData(csvArrays) {
     trainee.nationality = traineeArray[4];
     trainee.team = traineeArray[5];
     trainee.birthyear = traineeArray[6];
-    trainee.eliminated = traineeArray[8] === 'e'; // sets trainee to be eliminated if 'e' appears in 6th col
+    trainee.eliminated = traineeArray[7] === 'e'; // sets trainee to be eliminated if 'e' appears in 6th col
     trainee.top7 = traineeArray[7] === 't'; // sets trainee to top 12 if 't' appears in 6th column
     trainee.id = parseInt(traineeArray[7]) - 1; // trainee id is the original ordering of the trainees in the first csv
     trainee.image =
